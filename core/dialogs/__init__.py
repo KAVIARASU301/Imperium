327b9a87d3e4f3696e6b51f36062dc86fbc24349
KAVIARASU301 <kaviarasu301@gmail.com>
2026-03-05 10:00:03 +0530

    fix import error

--- core/dialogs/__init__.py
+++ core/dialogs/__init__.py
@@ -7,10 +7,12 @@ from .order_history_dialog import OrderHistoryDialog
 from .pending_orders_dialog import PendingOrdersDialog
 from .performance_dialog import PerformanceDialog
 from .pnl_history_dialog import PnlHistoryDialog
-from .quick_order_dialog import QuickOrderDialog
+from .quick_order_dialog import QuickOrderDialog, QuickOrderMode
 from .settings_dialog import SettingsDialog
 from .strategy_builder_dialog import StrategyBuilderDialog
 from .watchlist_dialog import WatchlistDialog
+from .cvd_symbol_set_multi_chart_dialog import CVDSetMultiChartDialog
+from .fii_dii_dialog import FIIDIIDialog
 
 __all__ = [
     'MarketMonitorDialog',
@@ -22,7 +24,10 @@ __all__ = [
     'PerformanceDialog',
     'PnlHistoryDialog',
     'QuickOrderDialog',
+    'QuickOrderMode',
     'SettingsDialog',
     'StrategyBuilderDialog',
-    'WatchlistDialog'
+    'WatchlistDialog',
+    'CVDSetMultiChartDialog',
+    'FIIDIIDialog'
 ]
