cea1ad4d45c81b37099289d9fa732d256250ced2
KAVIARASU301 <kaviarasu301@gmail.com>
2026-03-05 10:05:03 +0530

    update

--- core/widgets/__init__.py
+++ core/widgets/__init__.py
@@ -1,11 +1,18 @@
 """Widget components for Options Scalper"""
 
 from .account_summary import AccountSummaryWidget
+from .menu_bar import create_menu_bar
 from .order_status_widget import OrderStatusWidget
 from .performance_widget import PerformanceWidget
+from .strike_ladder import StrikeLadderWidget
+from positions_table import PositionsTable
 
 
 __all__ = [
+    'create_menu_bar',
     'AccountSummaryWidget',
-    'OrderStatusWidget'
+    'OrderStatusWidget',
+    'PerformanceWidget',
+    'PositionsTable',
+    'StrikeLadderWidget',
 ]
